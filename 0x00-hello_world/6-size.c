3a2b12e7ef08c5543b6cd3746b7be17eb16c0a12
juandsuarezz <1486@holbertonschool.com>
2020-02-05 17:44:00 +0000

    variables correct

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,15 +6,10 @@
  */
 int main(void)
 {
-char c;
-int i;
-long int li;
-long long int lli;
-float f;
-printf("Size of a char: %ld byte(s)\n", sizeof(c));
-printf("Size of a int: %ld byte(s)\n", sizeof(i));
-printf("Size of a long int: %ld byte(s)\n", sizeof(li));
-printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
-printf("Size of a float: %ld byte(s)\n", sizeof(f));
+printf("Size of a char: %ld byte(s)\n", sizeof(char));
+printf("Size of a int: %ld byte(s)\n", sizeof(int));
+printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+printf("Size of a float: %ld byte(s)\n", sizeof(float));
 return (0);
 }
