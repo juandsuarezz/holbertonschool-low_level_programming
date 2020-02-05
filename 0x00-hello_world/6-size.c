876472e8459ca0226c5f6144ed2b68dc3e8fd67e
juandsuarezz <1486@holbertonschool.com>
2020-02-05 17:36:36 +0000

    variables

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,6 +6,11 @@
  */
 int main(void)
 {
+char c;
+int i;
+long int li;
+long long int lli;
+float f;
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
 printf("Size of a int: %lu byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
