9cae99f505751eb6afba30e447a09e6c90437ec3
juandsuarezz <1486@holbertonschool.com>
2020-02-06 17:15:50 +0000

    correct

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,10 @@
 #include <time.h>
 /**
  *main - Entry point
- *Description
- *Return
+ *
+ *Description: This program will take a random number and display whether it is
+ *positive, negative, or zero.
+ *Return: Returns 0 upon successful run.
  */
 int main(void)
 {
