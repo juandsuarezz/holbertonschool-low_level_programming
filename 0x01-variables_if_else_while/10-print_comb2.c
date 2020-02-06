6955fbdd59c05c2fe8d9fd08f7078c9666878ace
juandsuarezz <60364795+juandsuarezz@users.noreply.github.com>
2020-02-06 17:58:10 -0500

    Update 10-print_comb2.c

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,45 +1,23 @@
 #include <stdio.h>
-
-/**                                       
-
- *main - Entry point                               
-
- *Description: Function                             
-
- *Return: 0 upon successful                           
-
+/**
+ *main - Entry point
+ *Description: Something
+ *Return: 0 upon successful
  */
-
 int main(void)
-
 {
-
 int c = 0;
-
 while (c < 100)
-
 {
-
 putchar((c / 10) + '0');
-
 putchar((c % 10) + '0');
-
 {
-
 if (c != 99)
-
 putchar(',');
-
 putchar(' ');
-
 }
-
 c++;
-
 }
-
 putchar('\n');
-
 return (0);
-
-}
+} 
