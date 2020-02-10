801407a2f3d0afefd6772b6e28f9747714e8e795
juandsuarezz <1486@holbertonschool.com>
2020-02-10 19:18:11 +0000

    first update

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int _abs(int n)
+{
+if (n >= 0)
+return (n);
+else
+return (-n);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ *main - Entry point
+ *
+ *Description: This program will print all letters in lowercase except for
+ *q and e.
+ *Return: 0 upon successful completion
+ */
+int print_last_digit(int num)
+{
+int lastdigit = num % 10;
+if (lastdigit < 0)
+{
+_putchar(-lastdigit + '0');
+return (-lastdigit);
+}
+else
+{
+_putchar(lastdigit + '0');
+return (lastdigit);
+}
+}
