abc5304a99a22d5d5034da90dccce40a3e62ba0d
juandsuarezz <60364795+juandsuarezz@users.noreply.github.com>
2020-02-18 19:22:22 -0500

    Create 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _strlen - Function that returns the lenght of a string.
+ * @s: Char as string
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int x;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+	}
+	return (x);
+}
