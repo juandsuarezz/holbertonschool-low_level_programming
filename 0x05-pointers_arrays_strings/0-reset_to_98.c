e2309425f5d4dcaf55fe1296a5dfd5b544ac9510
juandsuarezz <60364795+juandsuarezz@users.noreply.github.com>
2020-02-18 19:12:30 -0500

    Create 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Reset value of pointer to 98.
+ * @n: Integer as a pointer.
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+
+
+}
