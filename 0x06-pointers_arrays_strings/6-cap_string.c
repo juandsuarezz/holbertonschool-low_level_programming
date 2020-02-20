c6ff256bebcfc566a8cc9b41f193766b72e7ab8c
juandsuarezz <1486@holbertonschool.com>
2020-02-20 18:29:24 +0000

    first update

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ *cap_string - updates a integer value to 98
+ *@str: A pointer to an int that will be updated
+ *
+ *Return: void
+ */
+char *cap_string(char *str)
+{
+char *c = str;
+while (*str != '\0')
+{
+if (*str >= 'a' && *str <= 'z')
+if  (
+*(str - 1) == ' ' ||
+*(str - 1) == '\t' ||
+*(str - 1) == '\n' ||
+*(str - 1) == ',' ||
+*(str - 1) == ';' ||
+*(str - 1) == '.' ||
+*(str - 1) == '!' ||
+*(str - 1) == '?' ||
+*(str - 1) == '"' ||
+*(str - 1) == '(' ||
+*(str - 1) == ')' ||
+*(str - 1) == '{' ||
+*(str - 1) == '}' 
+)
+*str = *str - 32;
+str++;
+}
+return (c);
+}
