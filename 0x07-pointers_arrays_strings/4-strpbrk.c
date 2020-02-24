183e3152e6e9a94db53265e4008eb1df6b008fbf
juandsuarezz <1486@holbertonschool.com>
2020-02-24 17:04:09 +0000

    trail space

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -14,10 +14,10 @@ for (a = 0 ; s[a] ; a++)
 for (b = 0 ; accept[b] ; b++)
 {
 if (accept[b] == s[a])
-return (s+a);
+return (s + a);
 }
 if (accept[b] == s[a])
-return (s+a);
+return (s + a);
 }
 return (0);
 }
