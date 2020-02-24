5310ae2919df1bb0e1180db26479c4b17befa90f
juandsuarezz <1486@holbertonschool.com>
2020-02-24 19:59:21 +0000

    first update

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * print_diagsums - entry point
+ * 
+ * @a: array
+ * @size: array
+ *
+ */
+void print_diagsums(int *a, int size)
+{
+int i;
+int j;
+int s1 = 0;
+int s2 = 0;
+int c = 0;
+for(i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+if (i + j == size - 1)
+s2 = s2 + a[c];
+if (i == j)
+s1 = s1 + a[c];
+c++;
+}
+}
+printf ("%d,", s1);
+printf (" %d", s2);
+putchar ('\n');
+}
