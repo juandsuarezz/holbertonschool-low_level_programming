0643a6e0e3d46cdb4045f116171f1227221de0c2
juandsuarezz <1486@holbertonschool.com>
2020-02-26 12:51:38 +0000

    first update

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _puts_recursion - entry point
+ * Return: array
+ * @s: string
+ */
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+_putchar ('\n');
+return;
+}
+_putchar (*s);
+s++;
+_puts_recursion(s);
+}
