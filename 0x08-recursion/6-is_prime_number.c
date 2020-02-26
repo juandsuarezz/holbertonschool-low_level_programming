91052c1a2f8243ae1051adc00e3febfefd270ab0
juandsuarezz <1486@holbertonschool.com>
2020-02-26 18:53:06 +0000

    first update

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * check - entry point
+ * Return: array
+ * @count: counter
+ * @n: integer
+ */
+int check(int count, int n)
+{
+if (n == count)
+return (1);
+if (n <= 1)
+return (0);
+if (n % count == 0)
+return (0);
+return (check(count + 1, n));
+}
+/**
+ * is_prime_number - entry point
+ * Return: array
+ * @n: string
+ */
+int is_prime_number(int n)
+{
+return (check(2, n));
+}
