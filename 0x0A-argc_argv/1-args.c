b6e5672cab239bb91277d0034d89b8898f04b672
juandsuarezz <1486@holbertonschool.com>
2020-02-28 15:24:16 +0000

    first update

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: variable
+ * @argv: Variable
+ * Return: Always 0 (Success)
+ */
+int main (int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc - 1);
+return 0;
+}
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: variable
+ * @argv: Variable
+ * Return: Always 0 (Success)
+ */
+int main (int argc, char *argv[])
+{
+int count;
+printf ("%s\n",argv[0]);
+if (argc > 1)
+{
+for (count = 1; count < argc; count++)
+{
+printf("%s\n",argv[count]);
+}
+}
+return 0;
+}
