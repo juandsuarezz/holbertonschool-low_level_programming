04239882c0989bb763af5a728d31f187a6a7f443
juandsuarezz <1486@holbertonschool.com>
2020-02-28 15:00:34 +0000

    first update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: variable
+ * @argv: Variable
+ * Return: Always 0 (Success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n",argv[0]);
+return (0);
+}
