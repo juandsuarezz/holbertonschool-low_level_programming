368645d3bbcd95fe5f89f0e5319f15d7bd0addf8
juandsuarezz <1486@holbertonschool.com>
2020-03-03 19:26:08 +0000

    length corrected

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,22 +9,26 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-int con1;
-int con2;
+int con1 = 0;
+int con2 = 0;
 char *conar;
-if (s2 == NULL)
-{
-s2 = malloc(sizeof(char));
-*s2 = '\0';
-}
 if (s1 == NULL)
 {
-s1 = malloc(sizeof(char));
-*s1 = '\0';
+s1 = "";
 }
-conar = malloc(sizeof(s1) + sizeof(s2));
-if (conar != NULL)
+if (s2 == NULL)
 {
+s2 = "";
+}
+while (s1[con1] != '\0')
+con1++;
+while (s2[con2] != '\0')
+con2++;
+conar = malloc(sizeof(char) * (con1)+(con2 + 1));
+if (conar == NULL)
+{
+return (NULL);
+}
 for (con1 = 0; s1[con1] != '\0'; con1++)
 {
 conar[con1] = s1[con1];
@@ -33,8 +37,6 @@ for (con2 = 0; s2[con2] != '\0'; con1++, con2++)
 {
 conar[con1] = s2[con2];
 }
+conar[con1] = '\0';
 return (conar);
-}
-else
-return (NULL);
 }
