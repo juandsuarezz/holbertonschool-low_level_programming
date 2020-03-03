a128339a1f1869689849999efb858a8be8b98121
juandsuarezz <1486@holbertonschool.com>
2020-03-03 14:38:20 +0000

    first update

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *create_array(unsigned int size, char c)
+{
+unsigned int count = 0;
+char *array;
+if (size == 0)
+{
+return (NULL);
+}
+array = malloc(sizeof(char) * size);
+if (array == NULL)
+{
+return (NULL);
+}
+while (count < size)
+{
+array[count] = c;
+count++;
+}
+return(array);
+}
