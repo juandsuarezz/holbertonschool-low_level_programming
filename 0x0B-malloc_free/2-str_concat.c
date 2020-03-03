8920afe21debabd4b4277bb6e37bb90486073a36
juandsuarezz <1486@holbertonschool.com>
2020-03-03 18:04:45 +0000

    fix null

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,33 +9,32 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-  int con1;
-  int con2;
-  char *conar;
-
-  if (s2 == NULL)
-    {
-      s2 = malloc(sizeof(char));
-      *s2 = '\0';
-    }
-  if (s1 == NULL)
-    {
-      s1 = malloc(sizeof(char));
-      *s1 = '\0';
-    }
-  conar = malloc(sizeof(s1) + sizeof(s2));
-  if (conar != NULL)
-    {
-      for (con1 = 0; s1[con1] != '\0'; con1++)
-	{
-	  conar[con1] = s1[con1];
-	}
-      for (con2 = 0; s2[con2] != '\0'; con1++, con2++)
-	{
-	  conar[con1] = s2[con2];
-	}
-      return (conar);
-    }
-  else
-    return (NULL);
+int con1;
+int con2;
+char *conar;
+if (s2 == NULL)
+{
+s2 = malloc(sizeof(char));
+*s2 = '\0';
+}
+if (s1 == NULL)
+{
+s1 = malloc(sizeof(char));
+*s1 = '\0';
+}
+conar = malloc(sizeof(s1) + sizeof(s2));
+if (conar != NULL)
+{
+for (con1 = 0; s1[con1] != '\0'; con1++)
+{
+conar[con1] = s1[con1];
+}
+for (con2 = 0; s2[con2] != '\0'; con1++, con2++)
+{
+conar[con1] = s2[con2];
+}
+return (conar);
+}
+else
+return (NULL);
 }
