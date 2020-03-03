203b4a8f93b37e3f51f0131d9ca409afd0014152
juandsuarezz <1486@holbertonschool.com>
2020-03-03 16:10:55 +0000

    first update

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * strdup - strdup
+ * _strdup - _strdup
  * @str: variable
  * Return: Always 0 (Success)
  */
@@ -12,7 +12,7 @@ int i = 0;
 int lenght = 0;
 char *string;
 if (str == NULL)
-return(NULL);
+return (NULL);
 while (str[i] != '\0')
 {
 lenght++;
@@ -21,8 +21,8 @@ i++;
 i = 0;
 string = malloc(sizeof(char) * (lenght + 1));
 if (str == NULL)
-return(NULL);
- while (i < (lenght + 1))
+return (NULL);
+while (i < (lenght + 1))
 {
 string[i] = str[i];
 i++;
