f85e5fa825eadabc5f87f454e4ae48045af4bfc9
juandsuarezz <1486@holbertonschool.com>
2020-03-05 17:45:48 +0000

    conditional correct

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,39 +10,37 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int con1 = 0;
-unsigned int con2 = 0;
-char *conar;
-if (s1 == NULL)
-{
-s1 = "";
-}
-if (s2 == NULL)
-{
-s2 = "";
-}
-while (s1[con1] != '\0')
-con1++;
-if (con2 < n)
-{
-while (s2[con2])
-con2++;
-}
-while (s2[con2] != '\0')
-con2++;
-conar = malloc(sizeof(char) * (con1)+(con2 + 1));
-if (conar == NULL)
-{
-return (NULL);
-}
-for (con1 = 0; s1[con1] != '\0'; con1++)
-{
-conar[con1] = s1[con1];
-}
-for (con2 = 0; con2 < n; con1++, con2++)
-{
-conar[con1] = s2[con2];
-}
-conar[con1] = '\0';
-return (conar);
+	unsigned int con1 = 0;
+	unsigned int con2 = 0;
+	char *conar;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[con1] != '\0')
+		con1++;
+
+	while (s2[con2])
+		con2++;
+
+	if (n > con2)
+		n = con2;
+	conar = malloc(sizeof(char) * (con1) + (con2 + 1));
+	if (conar == NULL)
+	{
+		return (NULL);
+	}
+	for (con1 = 0; s1[con1] != '\0'; con1++)
+	{
+		conar[con1] = s1[con1];
+	}
+	for (con2 = 0; con2 < n; con1++, con2++)
+	{
+		conar[con1] = s2[con2];
+	}
+	conar[con1] = '\0';
+	return (conar);
 }
