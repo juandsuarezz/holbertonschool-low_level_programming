51fbf05042f0321956ed0e3aeeaaf3a3f9cb6085
juandsuarezz <1486@holbertonschool.com>
2020-03-05 15:31:16 +0000

    description fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,18 +2,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * str_concat - Concatenates a string.
- * @s1: String 1
- * @s2: String 2
+ * _calloc - Concatenates a string.
+ * @nmemb: String 1
+ * @size: String 2
  * Return: Null or the array concatenate.
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-unsigned int *array = NULL;
-if (nmemb == 0 || size == 0)
-{
-return (NULL);
-}
- array = malloc (sizeof(unsigned int) * (nmemb * size));
-return (array);
+	unsigned int *array = NULL;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	array = malloc(sizeof(unsigned int) * (nmemb * size));
+	return (array);
 }
--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -2,15 +2,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * str_concat - Concatenates a string.
- * @s1: String 1
- * @s2: String 2
+ * array_range - Concatenates a string.
+ * @min: String 1
+ * @max: String 2
  * Return: Null or the array concatenate.
  */
 int *array_range(int min, int max)
 {
-int *array;
+	int counter;
+	int *array;
 
-if (min > max)
-return (NULL);
+	if (min > max)
+		return (NULL);
 
+	array = malloc(sizeof(int) * (max - min + 1));
+
+	if (array == NULL)
+		return (NULL);
+
+	for (counter = 0; min <= max; min++, counter++)
+		*(counter + array) = min;
+
+	return (array);
+}
