8669d1cd34f7f853e59116c13d476fe417b42d53
juandsuarezz <1486@holbertonschool.com>
2020-03-05 14:39:38 +0000

    first update

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * str_concat - Concatenates a string.
+ * @s1: String 1
+ * @s2: String 2
+ * Return: Null or the array concatenate.
+ */
+int *array_range(int min, int max)
+{
+int *array;
+
+if (min > max)
+return (NULL);
+
