e047d3a68b48ead854f9a3f4a88e84eb896a153e
juandsuarezz <1486@holbertonschool.com>
2020-03-13 14:33:48 +0000

    first update

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * sum_them_all - Entry point
+ * @n: variable
+ * Return: sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i, sum;
+
+	va_start(ap, n);
+
+	sum = 0;
+	i= 0;
+
+	while (i < n)
+	{
+		sum = sum + va_arg(ap, int);
+		i++;
+	}
+	va_end(ap);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list numlist;
+	unsigned int i = 0;
+	int num = 0;
+
+	if (separator == NULL)
+		return;
+
+	va_start(numlist, n);
+
+	while (i < n)
+	{
+		num = va_arg(numlist, unsigned int);
+		printf("%i", num);
+		if (i < n - 1)
+		{
+			printf("%s", separator);
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(numlist);
+}
