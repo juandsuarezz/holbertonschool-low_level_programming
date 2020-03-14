002c6022d1373d59eabbe7b43ca8ee9a334d752a
juandsuarezz <60364795+juandsuarezz@users.noreply.github.com>
2020-03-14 17:52:17 -0500

    Loop corrected

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,24 +1,24 @@
-#include <stdarg.h>
 #include "variadic_functions.h"
+#include <stdarg.h>
 /**
- * sum_them_all - Entry point
- * @n: variable
- * Return: sum
+ * sum_them_all - function that returns the sum of all its parameters
+ * @n: Num of parameters
+ * Return: Sum of all the parameters
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	unsigned int x;
 	va_list ap;
-	unsigned int i, sum;
+	unsigned int sum = 0;
 
+	if (n == 0)
+	{
+		return (0);
+	}
 	va_start(ap, n);
-
-	sum = 0;
-	i = 0;
-
-	while (i < n)
+	for (x = 0; x < n; x++)
 	{
 		sum = sum + va_arg(ap, int);
-		i++;
 	}
 	va_end(ap);
 	return (sum);
