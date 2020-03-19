27b7b2fed1bee7f41784f1ddd4015abcf1d5f439
juandsuarezz <1486@holbertonschool.com>
2020-03-19 17:34:30 +0000

    add functions

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef __LISTS__
-#define __LISTS__
+#ifndef LISTS_H
+#define LISTS_H
 
 /**
  * struct list_s - singly linked list
@@ -22,6 +22,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-void printoverride(void) __attribute__((constructor));
 int _putchar(char c);
+
 #endif
