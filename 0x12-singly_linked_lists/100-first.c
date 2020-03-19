ecefbcd86a2815d3c20b3c7fef788ca32ae05cc8
juandsuarezz <1486@holbertonschool.com>
2020-03-19 19:35:34 +0000

    betty style

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@
  */
 void printbeforemain(void)
 {
-  printf("You're beat! and yet, you must allow,\n");
-  printf("I bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
