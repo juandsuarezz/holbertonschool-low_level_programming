d1811c1730858ed6c50cd8483bf9fddcc7589f06
juandsuarezz <1486@holbertonschool.com>
2020-03-19 17:23:18 +0000

    first update

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ *print_list - prints elements of list
+ *@h: a list
+ *
+ *Return: number of elements
+ *
+ */
+size_t print_list(const list_t *h)
+{
+int elements = 0;
+while (h != NULL)
+{
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+h = h->next;
+}
+else
+{
+printf("[%u] %s\n", h->len, h->str);
+h = h->next;
+}
+elements++;
+}
+return (elements);
+}
