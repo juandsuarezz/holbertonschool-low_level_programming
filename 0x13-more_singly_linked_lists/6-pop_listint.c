cd8fa32df0da1b54c5a6976f8a3cf0deae6aea71
juandsuarezz <1486@holbertonschool.com>
2020-03-24 14:10:13 +0000

    trail spaces

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -6,16 +6,13 @@
  */
 int pop_listint(listint_t **head)
 {
-  int number;
-  listint_t *newhead;
-
-  if (*head == NULL)
-    return (0);
-
-  number = (*head)->n;
-
-  newhead = (*head)->next;
-  free(*head);
-  *head = newhead;
-  return (number);
+int number;
+listint_t *newhead;
+if (*head == NULL)
+return (0);
+number = (*head)->n;
+newhead = (*head)->next;
+free(*head);
+*head = newhead;
+return (number);
 }
