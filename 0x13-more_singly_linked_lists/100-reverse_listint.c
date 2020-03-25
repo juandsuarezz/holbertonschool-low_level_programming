09700eb100f06e34428ea19487063a2f3c4dd05f
juandsuarezz <1486@holbertonschool.com>
2020-03-25 15:23:10 +0000

    add null

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -6,23 +6,15 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-listint_t *current;
+listint_t *current = NULL;
 listint_t *nxt;
-if (head == NULL || *head == NULL)
-return (NULL);
-if ((*head)->next == NULL)
-return (*head);
-current = *head;
-*head = current->next;
-nxt = (*head)->next;
-current->next = NULL;
-while (nxt != NULL)
+ while (*head != NULL)
 {
+nxt = (*head)->next;
 (*head)->next = current;
 current = *head;
 *head = nxt;
-nxt = nxt->next;
 }
-(*head)->next = current;
+*head = current;
 return (*head);
 }
