d12d22a896b7f4690dee0e20eed9fdbd3e5910ec
juandsuarezz <1486@holbertonschool.com>
2020-05-02 18:34:37 +0000

    add function that add new node at the beginning

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -2,7 +2,7 @@
 /**
  * add_dnodeint - adds a new node at the beginning of a doubly linked list
  * @head: head of the current doubly linked list
- * @n: value of the new node
+ * @n: the value of the new node
  * Return: the address of the new element, or NULL if failed
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
